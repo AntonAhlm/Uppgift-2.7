a32de23e01bf093ac126e49d99bd6058fe59d4ec
Anton <>
2023-01-25 10:35:32 +0100

    Add project files.

---
+++ Uppgift 2.7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Skriv en 2 ord lång mening (utan punkt) ");
+
+            Console.Write("Mening : ");
+            string Mening = Console.ReadLine();
+
+            int  MellanslagIndex = Mening.IndexOf(" ");
+
+            string ord1 = Mening[..MellanslagIndex];
+            string ord2 = Mening[(MellanslagIndex+1)..];
+
+
+            Console.WriteLine(ord2 + " " + ord1);
+        }
+
+
+    }
+
+}
